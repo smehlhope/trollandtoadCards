f176da965a9b8d1ff7c5404d10bcf8869b8fb000
smehlhope <stephanie.mehlhope@gmail.com>
2016-03-06 19:54:41 -0800

    expanding search to set

--- app/models/card.rb
+++ app/models/card.rb
@@ -22,7 +22,8 @@ class Card < ActiveRecord::Base
 
 
   def self.search(search)
-	    where('edition LIKE ?', "%#{search}%")
+	  where('edition LIKE ?', "%#{search}%")
+	  where('set LIKE ?', "%#{search}%")
 	end
 
 	WillPaginate.per_page = 100
